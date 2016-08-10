9ddbf0d8c326e91c8bfced23650ce1443e9f316a
Byakuya <tsiry.private.bbdo@gmail.com>
2016-08-10 15:49:36 +0400

    few callbacks

--- main.lua
+++ main.lua
@@ -1,6 +1,34 @@
-function love.draw()
+function love.load(arg)
   -- body...
-  love.graphics.print("Hello world", 100, 100);
+  image = love.graphics.newImage('image.jpg')
+  love.graphics.setNewFont(12)
+  love.graphics.setColor(0, 0, 0, 1)
+  love.graphics.setBackgroundColor(255, 255, 255, 1)
 end
 
--- test --
+
+function love.draw()
+  -- body...
+  love.graphics.draw(image, imageX, imageY)
+  love.graphics.print('Click and drag around or use key', 100, 100)
+end
+
+function love.update(dt)
+  -- body...
+  if love.keyboard.isDown('UP') then
+    num = num + 100 * dt
+  end
+end
+
+function love.mousepressed(x, y, button, isTouch)
+  -- body...
+  if button == 1 then
+    imgX = x
+    imgY = y
+  end
+end
+
+
+function love.quit()
+  -- body...
+end
