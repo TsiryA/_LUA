534208e37f7f94221a99be58a2334a42b8649f5d
Byakuya <tsiry.private.bbdo@gmail.com>
2016-08-10 17:15:54 +0400

    main base

--- main.lua
+++ main.lua
@@ -1,33 +1,17 @@
 function love.load(arg)
-  -- body...
-  image = love.graphics.newImage('image.jpg')
-  love.graphics.setNewFont(12)
-  love.graphics.setColor(0, 0, 0, 1)
-  love.graphics.setBackgroundColor(255, 255, 255, 1)
-end
+  -- Load medias
 
-
-function love.draw()
-  -- body...
-  love.graphics.draw(image, imageX, imageY)
-  love.graphics.print('Click and drag around or use key', 100, 100)
+  -- Load variables
 end
 
 function love.update(dt)
-  -- body...
-  if love.keyboard.isDown('UP') then
-    num = num + 100 * dt
-  end
+
 end
 
-function love.mousepressed(x, y, button, isTouch)
-  -- body...
-  if button == 1 then
-    imgX = x
-    imgY = y
-  end
+function love.draw()
+  -- Just test
+  love.graphics.print('Text to test', 100, 100)
 end
-
 
 function love.quit()
   -- body...
