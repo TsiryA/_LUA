e72248a878ae3952810c5810d76bfc5bfbb587ed
Byakuya <radiasont@proximity.bbdo.fr>
2016-09-02 17:05:11 +0400

    images array loader

---
+++ loaders/images.lua
@@ -0,0 +1,14 @@
+require "../modules/helpers"
+-- racine of images path
+racinePath = 'medias/imgs/'
+
+-- object of storage
+imageStore = {}
+-- function to add files in a folder into an array
+
+-- images for players
+playerPath = racinePath .. 'players/'
+-- add all players image file to player array
+imageStore.playersImg = helpers.loadImages(playerPath)
+
+-- images for clouds
