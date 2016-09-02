6b31cf9dafb3889ea10a5a8709e4b6f74a641997
Byakuya <radiasont@proximity.bbdo.fr>
2016-09-02 17:04:39 +0400

    image loader helper

---
+++ modules/helpers.lua
@@ -0,0 +1,15 @@
+helpers = {}
+
+-- load all images in the folder
+helpers.loadImages = function(folder)
+	local lfs = love.filesystem
+  fileArray = {}
+	local filesTable = lfs.getDirectoryItems(folder)
+	for i,v in ipairs(filesTable) do
+		local file = folder..'/'..v
+		if lfs.isFile(file) then
+			fileArray[#fileArray + 1] = love.graphics.newImage(file)
+		end
+	end
+	return fileArray
+end
